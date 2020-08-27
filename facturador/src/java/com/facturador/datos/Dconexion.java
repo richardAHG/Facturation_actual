db4111bdf57b1639cc10b86d6104e32f88b9f0d5
yuri <yuripako@gmail.com>
2020-08-27 13:57:16 -0500

    cree la clase coneccion

---
+++ facturador/src/java/com/facturador/datos/Dconexion.java
@@ -0,0 +1,47 @@
+
+package com.facturador.datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Dconexion {
+  
+   // Librería de MySQL
+    public static  String driver = "com.mysql.jdbc.Driver";
+
+    // Nombre de la base de datos
+    public static  String database = "dbfacturador";
+
+    // Host
+    public static  String hostname = "localhost";
+
+    // Puerto
+    public static  String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    public static  String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+    // Nombre de usuario
+    public static   String username = "root";
+
+    // Clave de usuario
+    public String password = "";
+   public Connection conectarMySQL() {
+        Connection con = null;
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+         
+        } catch (Exception e) {
+            System.out.println("ERROR CONECCION "+e);
+        }
+
+        return con;
+    }
+   
+ 
+   
+}
