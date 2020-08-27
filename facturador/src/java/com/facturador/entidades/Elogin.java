0f90db3e356867ae3d0d2215cba54ebf09972d12
yuri <yuripako@gmail.com>
2020-08-27 14:40:35 -0500

    creacion de clase entidades y negocio

---
+++ facturador/src/java/com/facturador/entidades/Elogin.java
@@ -0,0 +1,86 @@
+
+package com.facturador.entidades;
+
+public class Elogin {
+    
+    private int id;
+    private String nombre ;
+    private String apellidos;
+    private String email;
+    private String fecha;
+    private String user;
+    private String pass;
+
+    public Elogin() {
+    }
+
+    public Elogin(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", email=" + email + ", fecha=" + fecha + ", user=" + user + ", pass=" + pass + '}';
+    }
+
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+}
---
+++ facturador/src/java/com/facturador/negocios/Nlogin.java
@@ -0,0 +1,13 @@
+
+package com.facturador.negocios;
+
+import com.facturador.entidades.Elogin;
+
+
+public class Nlogin {
+
+private Elogin elogin = new Elogin();    
+
+    
+    
+}
